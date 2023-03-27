31dc423a057bc7ec85f679bced3df6157aedf636
Andrew Zadrozny <andrewza@email.unc.edu>
2023-03-27 11:35:21 -0400

    server test 1

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // Require http module
-const http = require('http');
+var http = require('http');
 // Require fs module
-const fs = require('fs');
+var fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 var minimist = require('minimist');
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const arg = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args["port"] || 3000;
+const port = args['port'] || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -17,12 +17,13 @@ const port = args["port"] || 3000;
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-fs.readFile("./public/index.html", (err, data => {
+var copy;
+fs.readFile('./public/index.html', (err, data => {
         if (err) {
                 console.error(err);
-                return
+                return;
         }
+	copy = data;
 }
 
 
